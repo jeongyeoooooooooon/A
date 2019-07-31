c7639d45df1b71bc8244b6c60261f878aedb2ef2
jeongyeon <joyr1226@likelion.org>
2019-07-31 22:52:20 +0900

    D

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from .form import BlogPost
 def home(request):
     blogs = Blog.objects.all().order_by('-id')
     #블로그의 모든 글들 대상으로
-    blog_list = Blog.objects.all()
+    blog_list = Blog.objects.all().order_by('-id')
     #블로그 객체 세 개를 한 페이지로 자르기
     paginator = Paginator(blog_list, 3)
     #request된 페이지가 뭔지를 알아내고 (request페이지를 변수에 담아내고)
